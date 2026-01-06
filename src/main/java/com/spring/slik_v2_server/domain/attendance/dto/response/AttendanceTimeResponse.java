01d314758d133d7de72d9b0f1e92f3b356f9a3b7
김경윤 <l982930009@gmail.com>
2026-01-06 15:54:40 +0900

    fix: 심자 당일 날짜 저장

--- src/main/java/com/spring/slik_v2_server/domain/attendance/dto/response/AttendanceTimeResponse.java
+++ src/main/java/com/spring/slik_v2_server/domain/attendance/dto/response/AttendanceTimeResponse.java
@@ -2,14 +2,16 @@ package com.spring.slik_v2_server.domain.attendance.dto.response;
 
 import com.spring.slik_v2_server.domain.attendance.entity.AttendanceTime;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 public record AttendanceTimeResponse(
+		LocalDate today,
 		LocalTime startTime,
 		LocalTime endTime
 ) {
 
 	public static AttendanceTimeResponse of(AttendanceTime attendanceTime) {
-		return new AttendanceTimeResponse(attendanceTime.getStartTime(), attendanceTime.getEndTime());
+		return new AttendanceTimeResponse(attendanceTime.getToday(), attendanceTime.getStartTime(), attendanceTime.getEndTime());
 	}
 }
