e18d18455ec5e0c557e0aa55b2aad3d1d32a151d
ParkJunSeok <jdksla0129@gmail.com>
2026-01-15 14:21:00 +0900

    feat:Request에 NotBlank어노테이션 추가

--- src/main/java/com/spring/slik_v2_server/domain/device/controller/DeviceController.java
+++ src/main/java/com/spring/slik_v2_server/domain/device/controller/DeviceController.java
@@ -4,6 +4,7 @@ import com.spring.slik_v2_server.domain.device.dto.request.FindAttendanceRequest
 import com.spring.slik_v2_server.domain.device.dto.request.UpdateDeviceRequest;
 import com.spring.slik_v2_server.domain.device.service.DeviceService;
 import com.spring.slik_v2_server.global.data.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,18 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/device/override")
+@RequestMapping("/device/override   ")
 public class DeviceController {
     private final DeviceService deviceService;
 
     @PostMapping("/lookup")
-    public ApiResponse<?> lookup(@RequestBody FindAttendanceRequest request) {
+    public ApiResponse<?> lookup(@Valid @RequestBody FindAttendanceRequest request) {
         return deviceService.findAttendanceStatus(request.studnet_id());
 
     }
 
     @PatchMapping("/confirm")
-    public ApiResponse<?> updateDevice(@RequestBody UpdateDeviceRequest request) {
+    public ApiResponse<?> updateDevice(@Valid @RequestBody UpdateDeviceRequest request) {
         return deviceService.updateAttendanceStatus(request);
     }
 }
--- src/main/java/com/spring/slik_v2_server/domain/device/dto/request/FindAttendanceRequest.java
+++ src/main/java/com/spring/slik_v2_server/domain/device/dto/request/FindAttendanceRequest.java
@@ -1,6 +1,9 @@
 package com.spring.slik_v2_server.domain.device.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record FindAttendanceRequest(
+        @NotBlank(message = "학번 입력을 해주세요.")
         String studnet_id
 ) {
 }
