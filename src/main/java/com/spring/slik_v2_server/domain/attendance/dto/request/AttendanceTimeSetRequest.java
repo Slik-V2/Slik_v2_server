70f1f1fe1c32a4ceee4d9dc4f18820b7b018a5a0
OH SEUNGYUN <happy1270111@gmail.com>
2026-01-21 13:09:18 +0900

    @JsonFormat 어노테이션 추가

--- src/main/java/com/spring/slik_v2_server/domain/attendance/dto/request/AttendanceTimeSetRequest.java
+++ src/main/java/com/spring/slik_v2_server/domain/attendance/dto/request/AttendanceTimeSetRequest.java
@@ -1,18 +1,28 @@
 package com.spring.slik_v2_server.domain.attendance.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalTime;
 
 public record AttendanceTimeSetRequest(
+		@JsonFormat(pattern = "HH:mm")
 		LocalTime session1_1Start,
+		@JsonFormat(pattern = "HH:mm")
 		LocalTime session1_1End,
 
+		@JsonFormat(pattern = "HH:mm")
 		LocalTime session1_2Start,
+		@JsonFormat(pattern = "HH:mm")
 		LocalTime session1_2End,
 
+		@JsonFormat(pattern = "HH:mm")
 		LocalTime session2_1Start,
+		@JsonFormat(pattern = "HH:mm")
 		LocalTime session2_1End,
 
+		@JsonFormat(pattern = "HH:mm")
 		LocalTime session2_2Start,
+		@JsonFormat(pattern = "HH:mm")
 		LocalTime session2_2End
 ) {
 }
