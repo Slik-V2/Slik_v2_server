14ce61d60bfd87b655e55ef0d75cfc2f5a027841
ParkJunSeok <jdksla0129@gmail.com>
2026-03-02 09:43:12 +0900

    refactor: Log저장방식을 비동기 처리 방식으로 수정

--- src/main/java/com/spring/slik_v2_server/domain/logs/service/LogsService.java
+++ src/main/java/com/spring/slik_v2_server/domain/logs/service/LogsService.java
@@ -3,8 +3,10 @@ package com.spring.slik_v2_server.domain.logs.service;
 import com.spring.slik_v2_server.domain.logs.dto.response.GetLogsResponse;
 import com.spring.slik_v2_server.domain.logs.entity.Logs;
 import com.spring.slik_v2_server.domain.logs.repository.LogsRepository;
+import com.spring.slik_v2_server.domain.student.entity.Student;
 import com.spring.slik_v2_server.global.data.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,5 +22,15 @@ public class LogsService {
         List<GetLogsResponse> responses = GetLogsResponse.fromList(logs);
 
         return ApiResponse.ok(responses);
+    }
+
+    @Async("logTaskExecutor")
+    public void saveLog(String action, String message, boolean success, Student student) {
+        logsRepository.save(Logs.builder()
+                .action(action)
+                .message(message)
+                .success(success)
+                .student(student)
+                .build());
     }
 }
